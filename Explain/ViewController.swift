6cab36c27966831f28d0fe35aaca55fe6636b3ba
cody <qudgus1984@naver.com>
2023-02-10 15:04:49 +0900

    [실습] just, from / Subscribe, bind 실습 완료

--- Explain/ViewController.swift
+++ Explain/ViewController.swift
@@ -12,20 +12,20 @@ import SwiftyJSON
 
 let MEMBER_LIST_URL = "https://my.api.mockaroo.com/members_with_avatar.json?key=44ce18f0"
 /*
-class Observable<T> {
-    private let task: (@escaping (T) -> Void) -> Void
-    
-    init(task: @escaping (@escaping (T) -> Void) -> Void) {
-        self.task = task
-    }
-    
-    func subscribe(_ f: @escaping (T) -> Void) {
-        task(f)
-    }
-}
-*/
+ class Observable<T> {
+ private let task: (@escaping (T) -> Void) -> Void
+ 
+ init(task: @escaping (@escaping (T) -> Void) -> Void) {
+ self.task = task
+ }
+ 
+ func subscribe(_ f: @escaping (T) -> Void) {
+ task(f)
+ }
+ }
+ */
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -98,69 +98,56 @@ class ViewController: UIViewController {
     // Bolt
     // RxSwift
     
+    // Observable의 생명주기
+    // 1. Create
+    // 2. Subscribe
+    // 3. onNext
+    // ---- 끝 ----
+    // 4. onCompleted / onError
+    // 5. Disposed
+    
     func downloadjson(_ url: String) -> Observable<String?> {
         // 1. 비동기로 생기는 데이터를 Observable로 감싸서 리턴하는 방법
-        Observable.create() { emitter in
-            let url = URL(string: url)!
-            let task = URLSession.shared.dataTask(with: url) { (data, _, err) in
-                guard err == nil else {
-                    emitter.onError(err!)
-                    return
-                }
-                
-                if let dat = data, let json = String(data: dat, encoding: .utf8) {
-                    emitter.onNext(json)
-                }
-                
-                emitter.onCompleted()
-            }
+            return Observable.from(["Hello", "World"])
+//            return Observable.create { emitter in
+//                emitter.onNext("Hello World")
+//                emitter.onCompleted()
+//                return Disposables.create()
             
-            task.resume()
-            
-            return Disposables.create() {
-                task.cancel()
-            }
-
-            
-            return Disposables.create()
         }
         
-//        return Observable.create { f in
-//            DispatchQueue.global().async {
-//                let url = URL(string: url)!
-//                let data = try! Data(contentsOf: url)
-//                let json = String(data: data, encoding: .utf8)
-//
-//                DispatchQueue.main.async {
-//                    f.onNext(json)
-//                }
-//            }
-//            return Disposables.create()
-//        }
-    }
+        
+        //        return Observable.create { f in
+        //            DispatchQueue.global().async {
+        //                let url = URL(string: url)!
+        //                let data = try! Data(contentsOf: url)
+        //                let json = String(data: data, encoding: .utf8)
+        //
+        //                DispatchQueue.main.async {
+        //                    f.onNext(json)
+        //                }
+        //            }
+        //            return Disposables.create()
+        //        }
+        //    }
     
     @objc func onLoad() {
         editView.text = ""
         setVisibleWithAnimation(activityIndicator, true)
         
         // 2. Observable로 오는 데이터를 받아서 처리하는 방법
-        let disposable = downloadjson(MEMBER_LIST_URL)
+        _ = downloadjson(MEMBER_LIST_URL)
             .subscribe { event in
                 switch event {
-                case let .next(json):
-                    DispatchQueue.main.sync {
-                        self.editView.text = json
-                        self.setVisibleWithAnimation(self.activityIndicator, false)
-                    }
-                    
+                case .next(let t):
+                    print(t)
+                    break
+                case .error(let err):
+                    break
                 case .completed:
                     break
-                case .error:
-                    break
                 }
-        }
-        
-        // disposable.dispose()
+            }
     }
 }
 
