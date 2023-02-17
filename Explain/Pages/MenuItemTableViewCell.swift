e7eb06c6807589562575fb03108eb6a947373fcb
cody <qudgus1984@naver.com>
2023-02-17 16:34:54 +0900

    [UI] UI Outlet Codebase로 변경

--- Explain/Pages/MenuItemTableViewCell.swift
+++ Explain/Pages/MenuItemTableViewCell.swift
@@ -6,13 +6,64 @@
 //
 
 import UIKit
+import SnapKit
 
 class MenuItemTableViewCell: UITableViewCell {
+    
+    public override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        configureUI()
+        configureLayout()
+        setConstraints()
+    }
+    
+    @available(*, unavailable)
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     var onCountChanged: (Int) -> Void = { _ in }
 
-    @IBOutlet var title: UILabel!
-    @IBOutlet var count: UILabel!
-    @IBOutlet var price: UILabel!
+//    @IBOutlet var title: UILabel!
+    internal var title = UILabel()
+//    @IBOutlet var count: UILabel!
+    internal var count = UILabel()
+//    @IBOutlet var price: UILabel!
+    internal var price = UILabel()
+    
+    func configureUI() {
+        title.font = UIFont.systemFont(ofSize: 24)
+        title.textColor = .black
+        count.font = UIFont.systemFont(ofSize: 18)
+        count.textColor = .systemGray3
+        price.textColor = .systemGray5
+        price.font = UIFont.systemFont(ofSize: 16)
+    }
+    
+    func configureLayout() {
+        [title, count, price].forEach { addSubview($0) }
+    }
+    
+    func setConstraints() {
+        price.snp.makeConstraints { make in
+            make.top.bottom.trailing.equalToSuperview()
+            make.width.equalTo(20)
+        }
+        
+        title.snp.makeConstraints { make in
+            make.top.bottom.leading.equalToSuperview()
+            make.width.equalTo(30)
+        }
+        
+        price.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.leading.equalTo(title.snp.trailing).offset(8)
+            make.width.equalTo(12)
+        }
+        
+    }
+    
 
     @IBAction func onIncreaseCount() {
         onCountChanged(1)
